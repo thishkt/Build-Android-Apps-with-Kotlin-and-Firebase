6ee7115e2d52c363ce4f72240648afee37fee983
HKT <thishkt@gmail.com>
2023-11-24 10:43:11 +0800

    Firebase Analytics 應用程式分析服務

--- app/src/main/java/com/hkt/firebase/MainActivity.kt
+++ app/src/main/java/com/hkt/firebase/MainActivity.kt
@@ -2,10 +2,25 @@ package com.hkt.firebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.analytics.logEvent
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //建立 FirebaseAnalytics 實例
+        firebaseAnalytics = Firebase.analytics
+
+        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_ITEM) {
+            param(FirebaseAnalytics.Param.ITEM_ID, "test id")
+            param(FirebaseAnalytics.Param.ITEM_NAME, "test name")
+            param(FirebaseAnalytics.Param.CONTENT_TYPE, "test image")
+        }
     }
 }
